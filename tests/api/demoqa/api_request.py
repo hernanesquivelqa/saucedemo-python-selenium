7602d777256f09cbf7d00281da0a95ac3ec89f14
hernan <hernan97carp@hotmail.com>
2025-03-07 17:06:44 -0300

    Added tests for deleting and posting new books via the API

--- tests/api/demoqa/api_request.py
+++ tests/api/demoqa/api_request.py
@@ -1,33 +1,78 @@
+import json
+import random
 import requests
-from config import USERNAME_DEMOQA, PASSWORD_DEMOQA
+from config import USERNAME_DEMOQA, PASSWORD_DEMOQA, USER_ID_DEMOQA, BEARER_DEMOQA
+
+
 class Api:
     def __init__(self):
-        self.base_url = 'https://demoqa.com'
+        self.base_url = "https://demoqa.com"
         self.api = requests
         self.endpoints = {
-            'create_user': '/Account/v1/User',
-            'generate_token': '/Account/v1/GenerateToken',
-            'login': '/Account/v1/Login',
-            'user': '/Account/v1/User/{username}',
-            'update_user': '/Account/v1/User/{username}'
+            "create_user": "/Account/v1/User",
+            "authorized": "/Account/v1/Authorized",
+            "generate_token": "/Account/v1/GenerateToken",
+            "get_books": "/BookStore/v1/Books",
+            "add_list_to_books": "/BookStore/v1/Books",
+            "delete_book_of_the_list": "/BookStore/v1/Book",
+            "login": "/Account/v1/Login",
+            "user": "/Account/v1/User/{username}",
+            "update_user": "/Account/v1/User/{username}",
         }
-        
-    def create_new_user(self, username, password):
-        url = self.base_url + self.endpoints['create_user']
-        request_data = {
-            "userName": username,
-            "password": password
-        }
-        response = self.api.post(url, data=request_data) 
-        #assert response.status_code == 201
-        body: object =  response.json()
-        return (body, response)
+        self.access_token = None
+        self.isbn_count = None
+
+    def create_new_user(self, username: str, password: str):
+        url = self.base_url + self.endpoints["create_user"]
+        request_data = {"userName": username, "password": password}
+        response = self.api.post(url, json=request_data)
+        # assert response.status_code == 201
+        body: object = response.json()
+        return body, response
+
+    def authorized(self):
+        url = self.base_url + self.endpoints["authorized"]
+        request_data = {"userName": USERNAME_DEMOQA, "password": PASSWORD_DEMOQA}
+        response = self.api.post(url, json=request_data)
+        body: object = response.json()
+        return body, response
+
     def set_access_token(self):
-        url = self.base_url + self.endpoints['generate_token']
-        request_data = {
-            "userName": USERNAME_DEMOQA,
-            "password": PASSWORD_DEMOQA
-        }
-        response = self.api.post(url, data=request_data) 
-        body: object =  response.json()
-        return (body, response)
+        url = self.base_url + self.endpoints["generate_token"]
+        request_data = {"userName": USERNAME_DEMOQA, "password": PASSWORD_DEMOQA}
+        response = self.api.post(url, json=request_data)
+        body: object = response.json()
+        self.access_token: str = body["token"]
+        return body, response
+
+    def get_books(self):
+        url = self.base_url + self.endpoints["get_books"]
+
+        response = self.api.get(url)
+        body: object = response.json()
+        self.isbn = body["books"][1]["isbn"]
+        self.isbn_count = len(body["books"])
+        return body, response
+
+    def delete_book(self, ids):
+        url = self.base_url + self.endpoints["delete_book_of_the_list"]
+        auth = {"Authorization": f"Bearer {self.access_token}"}
+
+        request_data = {"isbn": ids, "userId": USER_ID_DEMOQA}
+        response = self.api.delete(url, json=request_data, headers=auth)
+
+        # Si el código de respuesta es 204, retornamos un diccionario vacío
+        if response.status_code == 204:
+            return {}, response
+
+        # Para otros códigos de respuesta, intentamos parsear JSON
+        return response.json(), response
+
+    def add_list_to_books(self, ids):
+        url = self.base_url + self.endpoints["add_list_to_books"]
+        auth = {"Authorization": f"Bearer {self.access_token}"}
+        request_data = {"userId": USER_ID_DEMOQA, "collectionOfIsbns": ids}
+        response = self.api.post(url, json=request_data, headers=auth)
+        bodyText = response.text
+        body: object = json.loads(bodyText)
+        return body, response
--- tests/api/demoqa/test_books.py
+++ tests/api/demoqa/test_books.py
@@ -1,13 +1,53 @@
 import pytest
+from config import random_num
 from tests.api.demoqa.api_request import Api
 
+
 class TestBookApi:
-    def create_new_user(self):
+
+    def test_authorized(self):
         api = Api()
-        [body] = api.create_new_user("hernanasdk","Password1234!")
+        [json, response] = api.authorized()
+
+        assert response.status_code == 200
+        assert json == True
+
+    def test_generate_token(self):
+        api = Api()
+        [json, response] = api.set_access_token()
+        assert response.status_code == 200
+        assert json["token"] != None
+
+    def test_get_book(self):
+        api = Api()
+        [json, response] = api.get_books()
+
+        assert response.status_code == 200
+        assert json["books"] != None
+
+    pytest.mark.skip
+
+    def test_add_list_to_books(self):
+        api = Api()
+        api.set_access_token()
+        [json, response] = api.get_books()
+        book_id = json["books"][random_num]["isbn"]
+        data = [{"isbn": book_id}]
+        json, response = api.add_list_to_books(ids=data)
+        assert response.status_code == 201
+
+    pytest.mark.skip
+
+    def test_delete_book(self):
+        api = Api()
+        [json, response] = api.get_books()
+        book_id = json["books"][random_num]["isbn"]
+        api.set_access_token()
+
+        json, response = api.delete_book(ids=book_id)
+
+        assert response.status_code == 204
 
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     pytest.main()
